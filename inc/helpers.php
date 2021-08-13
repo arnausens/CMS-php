933d5d66eb5577aece1727282241379c0c8c852a
arnausens <arnaust3n11@gmail.com>
2021-08-13 14:48:54 +0200

    Finalizado proyecto cms PHP

---
+++ inc/helpers.php
@@ -0,0 +1,36 @@
+<?php 
+function redirect_to( $path ) {
+    header( 'Location:' . SITE_URL . '/' . $path);
+    die();
+}
+
+
+function generate_hash( $action ) {
+    return md5( $action );
+}
+
+function check_hash( $action , $hash ) {
+    if ( generate_hash( $action ) === $hash ) {
+        return true;
+    }
+    return false;
+}
+
+function is_logged_in() {
+    $is_user_logged_in = isset( $_SESSION['user']) && $_SESSION['user'] === ADMIN_USER;
+    return $is_user_logged_in;
+}
+
+function login($username, $password ) {
+    if ( $username === ADMIN_USER && $password === ADMIN_PASS) {
+		$_SESSION['user'] = ADMIN_USER;
+        return true;
+    }
+    return false;
+}
+
+function logout() {
+    unset( $_SESSION['user']);
+    redirect_to( 'index.php');
+    session_destroy();
+}
